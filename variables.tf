d1a703746d8b6d942a9c1441b5f350acf8078f1b
jordi.bru <jordi.bru@mango.com>
2024-05-27 20:35:44 +0200

    add: improved variables

--- variables.tf
+++ variables.tf
@@ -1,62 +1,64 @@
 # REQUIRED VARIABLES
 
 variable "stage" {
-  description = "Stage of development"
+  description = "The stage of development (e.g., test, dev, staging, prod)."
   type        = string
 
   validation {
-    condition     = can(regex("^(test|dev|stagin|prod)$", var.stage))
-    error_message = "Stage must be dev, staging or prod."
+    condition     = can(regex("^(test|dev|staging|prod)$", var.stage))
+    error_message = "Stage must be one of the following: test, dev, staging, prod."
   }
 }
 
 variable "purpose" {
-  description = "Short description about the created resource"
+  description = "A short description about the purpose of the created resource."
   type        = string
   default     = null
 
   validation {
     condition     = can(regex("^([a-zA-Z0-9-_]*)$", var.purpose))
-    error_message = "Only the expression [a-zA-Z0-9-_]* is allowed."
+    error_message = "Only the characters [a-zA-Z0-9-_] are allowed."
   }
 }
 
 variable "owner" {
-  description = "Owner of the deployed infrastructure"
+  description = "The owner of the deployed infrastructure."
   type        = string
   default     = null
 
   validation {
     condition     = length(var.owner) >= 3
-    error_message = "You must define an owner with more than three letters."
+    error_message = "The owner name must be at least three characters long."
   }
 }
 
 # CUSTOM VARIABLES
+
 variable "bucket_origin_id" {
-  description = "Bucket origin id"
+  description = "The ID of the hosting S3 bucket origin."
   type        = string
   default     = ""
 }
 
 variable "regional_domain" {
-  description = "Bucket domain name"
+  description = "The regional domain name of the hosting S3 bucket."
   type        = string
   default     = ""
 }
 
 variable "cert_id" {
-  description = "SSL Certificate arn to attach on the cloudfront distribution"
+  description = "The ARN of the SSL certificate to attach to the CloudFront distribution."
   type        = string
   default     = ""
 }
 
 variable "domain_name" {
-  description = "Name of the domain"
+  description = "The name of the domain."
   type        = string
   default     = ""
+
   validation {
     condition     = length(var.domain_name) >= 3
-    error_message = "You must define a domain name."
+    error_message = "You must define a domain name with at least three characters."
   }
 }
