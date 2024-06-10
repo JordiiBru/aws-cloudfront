40342bb53a57a058ae69bc417a16c48b071500ac
jordi.bru <jordi.bru@mango.com>
2024-06-10 16:45:23 +0200

    fix: rm oac

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-resource "aws_cloudfront_origin_access_control" "access_control" {
-  name                              = var.website_endpoint
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
-}
+# resource "aws_cloudfront_origin_access_control" "access_control" {
+#   name                              = var.website_endpoint
+#   origin_access_control_origin_type = "s3"
+#   signing_behavior                  = "always"
+#   signing_protocol                  = "sigv4"
+# }
 
 resource "aws_cloudfront_distribution" "main_distribution" {
   enabled             = true
